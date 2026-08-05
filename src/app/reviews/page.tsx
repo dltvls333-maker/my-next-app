b2b1cf08480229228282479e5b77aea18be983a6
your name <dltvls333@gmail.com>
2026-08-05 20:33:54 +0900

    rolling banner update112112121212dd1212111111121112121211212112121121212112adsas112231

--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -9,10 +9,10 @@ export default async function ReviewPage() {
   headers(); // Vercel 캐싱 방지 및 동적 서버 요청 강제
 
   // DB에서 데이터 전체 가져오기 (작성일 기준 내림차순)
-  const reviews = await prisma.review.findMany({
-    orderBy: { created_at: 'desc' },
-  });
-
+// page.tsx 수정
+const reviews = await prisma.review.findMany({
+  orderBy: { id: 'desc' }, // created_at 대신 id 기준 내림차순
+});
   console.log("DB에서 조회한 리뷰 데이터:", reviews);
 
   // DB 데이터를 컴포넌트가 사용하기 좋은 형태로 가공
