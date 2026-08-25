498586667524292a78d20d2965f715e20046e33a
your name <dltvls333@gmail.com>
2026-08-25 11:53:08 +0900

    ddd

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
-
+// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+// const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://wgpylcuigawjllhsbnqi.supabase.co";
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndncHlsY3VpZ2F3amxsaHNibnFpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4NjYwMjE1NywiZXhwIjoyMTAyMTc4MTU3fQ.LxykZFQjT4mvo5P_5PJ4S6t36skgCSntaPCj1BkOSRc";
 export const supabase = createClient(supabaseUrl, supabaseKey);
